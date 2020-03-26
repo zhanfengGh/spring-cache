f74b528255c7451c8159cb4e6f16ec0a25ee6758
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2020-03-26 10:21:19 +0800

    feat: eliminate duplicate config

--- src/main/java/com/feng/learn/github/springcache/infrastructure/mybatisplus/SomeRepositoryImpl.java
+++ src/main/java/com/feng/learn/github/springcache/infrastructure/mybatisplus/SomeRepositoryImpl.java
@@ -1,9 +1,10 @@
 package com.feng.learn.github.springcache.infrastructure.mybatisplus;
 
-import static com.feng.learn.github.springcache.infrastructure.spring.cache.SpringCacheConfig.JVM_CACHE_Manage;
+import static com.feng.learn.github.springcache.infrastructure.spring.cache.SpringCacheConfig.JVM_CACHE_MANAGE;
 
 import com.feng.learn.github.springcache.domain.model.SomeModel;
 import com.feng.learn.github.springcache.domain.repository.SomeRepository;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,12 +15,11 @@ import org.springframework.stereotype.Repository;
  * @date 2020/03/25
  */
 @Repository
+@CacheConfig(cacheNames = {"someCache"}, cacheManager = JVM_CACHE_MANAGE)
 public class SomeRepositoryImpl implements SomeRepository {
 
     @Override
-    @Cacheable(cacheManager = JVM_CACHE_Manage,
-        cacheNames = {"someCache", "anotherCache"},
-        key = "'/sm/' + #root.args[0]")
+    @Cacheable(key = "'/sm/' + #root.args[0]")
     public SomeModel getById(Long id) {
         try {
             // simulate loading From Db
@@ -30,15 +30,14 @@ public class SomeRepositoryImpl implements SomeRepository {
         return new SomeModel(id).setName("someModel");
     }
 
-    @CachePut(cacheManager = JVM_CACHE_Manage,
-        cacheNames = {"someCache", "anotherCache"},
+    @CachePut(cacheNames = {"someCache", "anotherCache"},
         key = "'/sm/' + #root.args[0]")
     @Override
     public int updateById(SomeModel someModel) {
         return 0;
     }
 
-    @CacheEvict(cacheManager = JVM_CACHE_Manage,
+    @CacheEvict(cacheManager = JVM_CACHE_MANAGE,
         cacheNames = {"someCache", "anotherCache"},
         key = "'/sm/' + #root.args[0]")
     @Override
--- src/main/java/com/feng/learn/github/springcache/infrastructure/spring/cache/SpringCacheConfig.java
+++ src/main/java/com/feng/learn/github/springcache/infrastructure/spring/cache/SpringCacheConfig.java
@@ -1,6 +1,5 @@
 package com.feng.learn.github.springcache.infrastructure.spring.cache;
 
-import java.util.concurrent.TimeUnit;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
@@ -15,9 +14,9 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class SpringCacheConfig {
 
-    public static final String JVM_CACHE_Manage = "jvmCacheManage";
+    public static final String JVM_CACHE_MANAGE = "jvmCacheManage";
 
-    @Bean(JVM_CACHE_Manage)
+    @Bean(JVM_CACHE_MANAGE)
     public CacheManager jvmCacheManager() {
         return new ConcurrentMapCacheManager();
     }
